ab6e4e7fa85d1577bc78b9c730c9018913b84588
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-03-11 10:22:23 -0500

    Create C++20.DanglingPointers.AndReferences.Example.h

---
+++ C++20.DanglingPointers.AndReferences.Example.h
@@ -0,0 +1,85 @@
+
+/** Q:Provide a c++20 example of a dangling reference, and an example of a dangling pointer.
+
+A:Sure, here are some examples of dangling references and pointers in C++20:
+
+Dangling Reference Example:
+
+c */
+
+#include <iostream>
+#include <string>
+ 
+std::string& get_string_ref()
+{
+    std::string s = "Hello World";
+    return s; // returning a reference to a local variable
+}
+ 
+int main()
+{
+    std::string& s_ref = get_string_ref();
+    std::cout << s_ref << std::endl; // accessing a dangling reference
+    return 0;
+}
+
+/** A:In this example, the function get_string_ref returns a reference to a local variable s, 
+which is destroyed when the function returns. As a result, s_ref becomes a dangling reference, 
+and accessing it leads to undefined behavior.
+
+Dangling Pointer Example:
+
+c */
+
+#include <iostream>
+#include <string>
+ 
+int* get_int_pointer()
+{
+    int num = 42;
+    int* ptr = &num;
+    return ptr; // returning a pointer to a local variable
+}
+ 
+int main()
+{
+    int* num_ptr = get_int_pointer();
+    std::cout << *num_ptr << std::endl; // accessing a dangling pointer
+    return 0;
+}
+
+/** A:In this example, the function get_int_pointer returns a pointer to a local variable num, 
+ which is destroyed when the function returns. As a result, num_ptr becomes a dangling pointer, 
+ and accessing it leads to undefined behavior. */
+
+/** Q:If I pass a parameter using a move reference to an unused parameter, does that create a dangling reference to that parameter?
+
+A:No, passing a parameter using a move reference to an unused parameter 
+does not create a dangling reference to that parameter.
+
+When you pass an argument to a function using a move reference (&&), 
+you are effectively transferring ownership of the object to the function. 
+If the function doesn't make use of the parameter, the parameter is simply ignored, 
+and there is no reference left to the object.
+
+For example:
+
+c */
+
+#include <iostream>
+#include <string>
+
+void do_something(std::string&& str) {
+    // do nothing with str
+}
+
+int main() {
+    std::string s = "Hello World";
+    do_something(std::move(s));
+    std::cout << s << std::endl; // s is still valid here
+    return 0;
+}
+
+/* In this example, we pass the string "s" to the function do_something using a move reference. 
+The function doesn't make use of the parameter, so it is simply ignored. After the function call, 
+"s" is still a valid object, and there is no dangling reference to it. */
