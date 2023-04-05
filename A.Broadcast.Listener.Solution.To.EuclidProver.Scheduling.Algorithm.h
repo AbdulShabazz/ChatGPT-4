c3ee940c36ac4c85ccd92e52de20ed4c97236ff9
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-04-05 05:55:00 -0400

    Create A.Broadcast.Listener.Solution.To.EuclidProver.Scheduling.Algorithm.h

---
+++ A.Broadcast.Listener.Solution.To.EuclidProver.Scheduling.Algorithm.h
@@ -0,0 +1,84 @@
+/* 
+Q:Is there a Listener/Broadcast configuration or architecture which could be a solution?
+
+Yes, a Listener/Broadcast (or Publisher/Subscriber) architecture can be a solution to avoid contention in accessing the unordered_map. In this pattern, objects (listeners/subscribers) register themselves to receive updates from a central source (broadcast/publisher) when a change occurs, instead of directly accessing the shared data structure. This reduces contention and avoids the need for fine-grained locking.
+
+Here's a high-level example of how you can implement a Listener/Broadcast architecture:
+
+    Define an AttributeListener interface and a Broadcaster class:
+
+cpp
+ */
+class AttributeListener {
+public:
+    virtual void onAttributeChanged(uint64_t key, uint64_t new_value) = 0;
+};
+
+class Broadcaster {
+public:
+    void addListener(AttributeListener* listener) {
+        listeners.push_back(listener);
+    }
+
+    void notifyAttributeChanged(uint64_t key, uint64_t new_value) {
+        for (auto listener : listeners) {
+            listener->onAttributeChanged(key, new_value);
+        }
+    }
+
+private:
+    std::vector<AttributeListener*> listeners;
+};
+/* 
+    Make your objects inherit from AttributeListener:
+
+cpp
+ */
+class MyObject : public AttributeListener {
+public:
+    MyObject(uint64_t key) : key(key) {}
+
+    void onAttributeChanged(uint64_t key, uint64_t new_value) override {
+        if (this->key == key) {
+            attribute = new_value;
+        }
+    }
+
+private:
+    uint64_t key;
+    uint64_t attribute;
+    // Other members...
+};
+/* 
+    Use the Broadcaster to propagate attribute changes:
+
+cpp
+ */
+int main() {
+    Broadcaster broadcaster;
+
+    // Create objects and register them as listeners
+    std::vector<MyObject> objects;
+    for (uint64_t i = 0; i < 1000; ++i) {
+        objects.emplace_back(i);
+        broadcaster.addListener(&objects.back());
+    }
+
+    // Update an attribute and broadcast the change
+    uint64_t key = 42;
+    uint64_t new_value = 12345;
+    broadcaster.notifyAttributeChanged(key, new_value);
+
+    return 0;
+}
+/* 
+In this example, the objects no longer need to access the shared unordered_map to read each other's attribute values. Instead, they receive updates through the onAttributeChanged method when an attribute changes. This reduces contention and simplifies synchronization.
+
+However, this approach has some trade-offs:
+
+    It increases memory usage since each object stores its attribute value.
+    It can increase communication overhead, as each update must be broadcast to all listeners.
+    It may not be suitable for scenarios where you need to query an object's attribute value at any given time, as the broadcast is only sent when an attribute is updated.
+
+You should consider these trade-offs when deciding if the Listener/Broadcast architecture is the right solution for your use case.
+ */
