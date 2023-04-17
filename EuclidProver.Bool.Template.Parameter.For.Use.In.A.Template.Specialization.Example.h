4b9537e4a2f6820ee7cf78db61cef80438779fc7
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-04-17 19:59:06 -0400

    Update EuclidProver.Bool.Template.Parameter.For.Use.In.A.Template.Specialization.Example.h

--- EuclidProver.Bool.Template.Parameter.For.Use.In.A.Template.Specialization.Example.h
+++ EuclidProver.Bool.Template.Parameter.For.Use.In.A.Template.Specialization.Example.h
@@ -22,3 +22,29 @@ int main()
 	// fn<1 == 1>(); // prints "Foo Bar Baz Qux: N must be 1.\n"
 	return 0;
 }
+
+//
+
+#include <iostream>
+
+template<bool N>
+void fn();
+
+template<>
+void fn<true>()
+{
+    std::cout << "Hello World!\n";   
+}
+
+template<>
+void fn<false>()
+{
+    std::cout << "Foo Bar Baz Qux: N must be 1.\n";
+}
+
+int main()
+{
+    fn<1 == 1>(); // prints "Hello World!"
+    // fn<1 == 1>(); // prints "Foo Bar Baz Qux: N must be 1.\n"
+    return 0;
+}
