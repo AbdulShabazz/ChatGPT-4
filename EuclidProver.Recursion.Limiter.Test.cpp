157d9b3397876eaee231da427819746c100d3880
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-04-17 12:46:10 -0400

    Create EuclidProver.Recursion.Limiter.Test.cpp

---
+++ EuclidProver.Recursion.Limiter.Test.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <thread>
+#include <atomic>
+#include <cstdint>
+#include <functional>
+
+const std::size_t num_threads = std::thread::hardware_concurrency() - 1; // Reserve, minus the main thread
+
+class RecursionLimiter
+{
+public:
+    static void set_recursion_limit(uint64_t limit)
+    {
+        recursion_limit = limit;
+    }
+
+    class Invoke
+    {
+    public:
+        Invoke(uint64_t initial_depth)
+            : depth(initial_depth),
+              recursion_count(thread_local_recursion_count())
+        {
+            if (depth)
+            {
+                *recursion_count += depth;
+            }
+        }
+
+        ~Invoke()
+        {
+            if (recursion_count)
+            {
+                *recursion_count -= depth;
+            }
+        }
+
+        bool acquired()
+        {
+            if (!recursion_count)
+            {
+                recursion_count = thread_local_recursion_count();
+                return true;
+            }
+
+            if (*recursion_count + depth <= recursion_limit)
+            {
+                *recursion_count += depth;
+                return true;
+            }
+
+            return false;
+        }
+
+    private:
+        uint64_t depth;
+        std::atomic<uint64_t>* thread_local_recursion_count()
+        {
+            thread_local std::atomic<uint64_t> rc{0};
+            return &rc;
+        }
+
+        std::atomic<uint64_t>* recursion_count;
+    };
+
+private:
+    static inline std::atomic<uint64_t> recursion_limit;
+};
+
+int main()
+{
+    RecursionLimiter::set_recursion_limit(32768); // 15-bits of depth on x86i64 platforms
+
+    uint64_t calllim{};
+    
+    std::function<void(void)> fn = [&calllim,&fn]()
+    {
+        RecursionLimiter::Invoke limiter(1);
+        if (limiter.acquired())
+        {
+            std::cout << "1 stack call made. Call limit: " << calllim++ << std::endl;
+        }
+        
+        fn();
+    };
+    
+    fn();
+    
+    return 0;
+}
+
