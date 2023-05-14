b9e0f6c41b838b7956aed07076580a18cbd4ab68
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-05-14 00:35:17 -0400

    Update ChatGPT.todo.h

--- ChatGPT.todo.h
+++ ChatGPT.todo.h
@@ -3,3 +3,28 @@ Update README examples
 Update Euclid.h
 Update main.h example
 */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <initializer_list>
+#include<cstdlib>
+void Theorem(std::initializer_list<std::string> args)
+{
+	std::vector<std::string> buff;
+	std::string s {};
+	for (const auto& arg : args)
+	{
+		s += arg;
+	}
+	std::cout << s << std::endl;
+};
+
+int main()
+{
+	Theorem({"2"});
+	Theorem({"4"});
+	Theorem({"2","+","2"});
+	Theorem({"1","+","1","+","1","+","1"});
+
+	return EXIT_SUCCESS;
+}
