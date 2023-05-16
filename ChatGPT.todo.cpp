1924fbf06ff1e7c1752c94da863211ef574f5da4
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-05-16 11:57:25 -0400

    Update ChatGPT.todo.cpp

--- ChatGPT.todo.cpp
+++ ChatGPT.todo.cpp
@@ -3,39 +3,25 @@
 #include <vector>
 #include <initializer_list>
 // input
-std::initializer_list<auto> Theorem_0 = 
+std::vector<
+std::vector<std::string>> Theorem_0 = 
 {
 	{"1","+","1","+","1","+","1"},
 	{"4"}
 };
 
-const auto Axiom_1 = 
+std::vector<
+std::vector<std::string>> Axiom_1 = 
 {
 	{"1","+","1"},
 	{"2"}
 };
 
-const auto Axiom_2 = 
+std::vector<
+std::vector<std::string>> Axiom_2 = 
 {
 	{"2","+","2"},
 	{"4"}
-};
-
-// output //
-auto Proofstep = 
-{
-	// {{lhs}, {rhs}} //
-	{{"1","+","1","+","1","+","1"},{"4"}},
-	{{"2","+","1","+","1"},{"4"}},
-	{{"2","+","2"},{"4"}},
-	{{"4"},{"4"}}
-};
-
-auto AxiomCommitLog = 
-{
-	"rhs_expansion via Axiom_2",
-	"rhs_expansion via Axiom_1",
-	"rhs_expansion via Axiom_1"
 };
 
 /**
@@ -65,20 +51,44 @@ auto AxiomCommitLog =
 */
 
 // formalized input for a recurrent neural network (ie. A 4D Vector) //
+std::vector<
+std::vector<
+std::vector<std::size_t>>> RouteMap3DVecUInt64 =
 {
-	// Theorem_0 //
+	// {{opcode,axiom_N},{lhs}, {rhs}} //
 	{{ 41,11 },{ 53,61,53,61,53,61,53 },{ 43 }},
-	// Axiom_1 //
 	{{ 43,53 },{ 57,61,57 },{ 41 }},
-	// Axiom_2 //
 	{{ 43,57 },{ 41,61,41 },{ 43 }}
-}
+};
 
 // formalized output for a recurrent neural network (ie. A 4D Vector) //
+std::vector<
+std::vector<
+std::vector<std::size_t>>> Result3DVecUInt64 =
 {
-	// {{op,axiom}{lhs}, {rhs}} //
+	// {{opcode,axiom_N},{lhs}, {rhs}} //
 	{{ 41,11,0 },{ 53,61,53,61,53,61,53 },{ 43 }}, // theorem_0 root: 1 + 1 + 1 + 1 = 4 //
 	{{ 7,43,57 },{ 57,61,53,61,53 },{ 43 }}, // rhs_expansion via axiom_2: 2 + 1 + 1 = 4 //
 	{{ 7,43,53 },{ 57,61,57 },{ 43 }}, // rhs_expansion via axiom_1: 2 + 2 = 4 //
 	{{ 7,43,53 },{ 59 },{ 59 }} // rhs_expansion via axiom_1: 4 = 4 // QED
-}
+};
+
+/**
+// output //
+auto Proofstep = 
+{
+	// {{lhs}, {rhs}} //
+	{{"1","+","1","+","1","+","1"},{"4"}},
+	{{"2","+","1","+","1"},{"4"}},
+	{{"2","+","2"},{"4"}},
+	{{"4"},{"4"}}
+};
+
+// output //
+auto AxiomCommitLog = 
+{
+	"rhs_expansion via Axiom_2",
+	"rhs_expansion via Axiom_1",
+	"rhs_expansion via Axiom_1"
+};
+*/
