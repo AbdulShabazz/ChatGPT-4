e67722b20450b14f783fa18b797d6ca714b89a18
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-05-20 11:42:48 -0400

    Create Custom.Hash.Algorithm.h

---
+++ Custom.Hash.Algorithm.h
@@ -0,0 +1,13 @@
+#include <vector>
+#include <functional>
+
+template <typename T>
+size_t hashVector(const std::vector<T>& vec) {
+    std::hash<T> hasher;
+    size_t hash = 0;
+    for (const auto& elem : vec) {
+        // Combine the hash of the current element with the hash so far using XOR and bit shifting
+        hash ^= hasher(elem) + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+    }
+    return hash;
+}
