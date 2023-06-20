6fe7df66d2df57b4dbc8e5735e7fd999ecfd9431
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-06-20 02:10:12 -0400

    Update A.NodeJS.Configuration.Example.js

--- A.NodeJS.Configuration.Example.js
+++ A.NodeJS.Configuration.Example.js
@@ -77,3 +77,67 @@ sh
 
 Please note that this example is intended for local development and learning purposes. If you're planning to deploy a Node.js application to a production environment, you should consider using a more robust configuration, which includes proper security measures, performance optimizations, and using a reverse proxy like Nginx.
 */
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(__dirname, 'public', req.url);
+    let contentType = 'text/html';
+    const extname = path.extname(filePath);
+
+    switch (extname) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'application/javascript';
+            break;
+        default:
+            contentType = 'text/html';
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(500);
+            res.end(`Server Error: ${err.code}`);
+        } else {
+           // res.setHeader("Content-Security-Policy", "script-src 'self'; style-src 'self'");
+            res.writeHead(200, { 'Content-Type': contentType });
+            
+            res.end(content, 'utf-8');
+        }
+    });
+});
+server.listen();
+
+//
+
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var message = 'It works!\n',
+        version = 'NodeJS ' + process.versions.node + '\n',
+        response = [message, version].join('\n');
+    res.end(response);
+});
+server.listen();
+
+//
+
+var express = require('express');
+var path = require('path');
+var app = express();
+
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
+
+// Route to serve the HTML file
+app.get(__dirname, (req, res) => {
+    res.sendFile(path.join(__dirname, 'public','index.html'));
+});
+
+// Start the server
+app.listen();
+
